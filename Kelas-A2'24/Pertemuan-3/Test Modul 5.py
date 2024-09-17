e217e56ad4af176119d65f5cfa57a478cf37697a
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-09-18 07:43:26 +0800

    Update Test Modul 5.py

---
+++ Kelas-A2'24/Pertemuan-3/Test Modul 5.py
@@ -0,0 +1,15 @@
+# kamu = "malas makan"
+# kamu = input("kamu kenyang, lapar, atau malas: ")
+# if kamu == "lapar":
+#     print("kamu pergi makan")
+# elif kamu == "kenyang":
+#     print("kamu pergi tidur")
+# elif kamu == "malas":
+#     print("kamu pemalas")
+# else:
+#     print("ga jelas lu")
+
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
+
