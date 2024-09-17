3af04abbc45a1234f4d197ad2ac71905be89cae4
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-09-18 07:55:50 +0800

    Update Test Modul 5.py

--- Kelas-A2'24/Pertemuan-3/Test Modul 5.py
+++ Kelas-A2'24/Pertemuan-3/Test Modul 5.py
@@ -13,3 +13,37 @@
 # result = "Genap" if angka % 2 == 0 else "Ganjil"
 # print(angka, "adalah angka",result)
 
+# print("""
+# ====================
+# Kalkulator Sederhana
+# 1.+
+# 2.-
+# 3.*
+# 4./
+# ====================
+# """)
+# fitur = int(input("pilih fitur : "))
+# match fitur :
+#     case 1:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a + b
+#         print(f"hasil penjumlahan angka 1 dan 2 adalah {c}")
+#     case 2:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a - b
+#         print(f"hasil pengurangan angka 1 dan 2 adalah {c}")
+#     case 3:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a * b
+#         print(f"hasil perkalian angka 1 dan 2 adalah {c}")
+#     case 4:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a / b
+#         print(f"hasil pembagian angka 1 dan 2 adalah {c}")
+#     case _:
+#         print("banyak banar")
+
