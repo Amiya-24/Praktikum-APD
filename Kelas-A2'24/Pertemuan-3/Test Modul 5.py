2b2f4896672b89bc8c22d8547287c21111f62ca1
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-09-18 15:03:51 +0800

    Update Test Modul 5.py

--- Kelas-A2'24/Pertemuan-3/Test Modul 5.py
+++ Kelas-A2'24/Pertemuan-3/Test Modul 5.py
@@ -1,3 +1,4 @@
+#                                                               PERCABANGAN
 # kamu = "malas makan"
 # kamu = input("kamu kenyang, lapar, atau malas: ")
 # if kamu == "lapar":
@@ -9,47 +10,48 @@
 # else:
 #     print("ga jelas lu")
 
-
+#                                                             TERNARY OPERATOR
 # angka = int(input("Masukkan angka: "))
 # result = "Genap" if angka % 2 == 0 else "Ganjil"
 # print(angka, "adalah angka",result)
 
+#                                                             KALKULATOR SIMPLE
+# print("""
+# ====================
+# Kalkulator Sederhana
+# 1.+
+# 2.-
+# 3.*
+# 4./
+# ====================
+# """)
 
-print("""
-====================
-Kalkulator Sederhana
-1.+
-2.-
-3.*
-4./
-====================
-""")
-fitur = int(input("pilih fitur : "))
-match fitur :
-    case 1:
-        a =int(input("Masukkan Angka 1 : "))
-        b =int(input("Masukkan Angka 2 : "))
-        c = a + b
-        print(f"hasil penjumlahan angka 1 dan 2 adalah {c}")
-    case 2:
-        a =int(input("Masukkan Angka 1 : "))
-        b =int(input("Masukkan Angka 2 : "))
-        c = a - b
-        print(f"hasil pengurangan angka 1 dan 2 adalah {c}")
-    case 3:
-        a =int(input("Masukkan Angka 1 : "))
-        b =int(input("Masukkan Angka 2 : "))
-        c = a * b
-        print(f"hasil perkalian angka 1 dan 2 adalah {c}")
-    case 4:
-        a =int(input("Masukkan Angka 1 : "))
-        b =int(input("Masukkan Angka 2 : "))
-        c = a / b
-        print(f"hasil pembagian angka 1 dan 2 adalah {c}")
-    case _:
-        print("banyak banar")
+# fitur = int(input("pilih fitur : "))
+# match fitur :
+#     case 1:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a + b
+#         print(f"hasil penjumlahan angka 1 dan 2 adalah {c}")
+#     case 2:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a - b
+#         print(f"hasil pengurangan angka 1 dan 2 adalah {c}")
+#     case 3:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a * b
+#         print(f"hasil perkalian angka 1 dan 2 adalah {c}")
+#     case 4:
+#         a =int(input("Masukkan Angka 1 : "))
+#         b =int(input("Masukkan Angka 2 : "))
+#         c = a / b
+#         print(f"hasil pembagian angka 1 dan 2 adalah {c}")
+#     case _:
+#         print("banyak banar")
 
-
+#                                                             CONTOH PERCABANGAN
 # nilai = int(input("Masukkan nilai anda : "))
 
 # if nilai > 100 :
