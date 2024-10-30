6b1b1e96078a0d463fef73d56654ad1ee5d5bf4c
Amiya-24 <muhammadfaizlazuardi4@gmail.com>
2024-10-30 16:27:15 +0800

    ga tau bener atau kadak

--- Post-Test/Project-Akhir/PA_Kelompok7.py
+++ Post-Test/Project-Akhir/PA_Kelompok7.py
@@ -6,12 +6,28 @@ def clean():
 
 clean()
 
-def register(username, password, role):
+def cek_username(username):
+    try:
+        with open("./Post-Test/Project-Akhir/akun.csv", "r") as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if row[0] == username:
+                    print("Username Sudah Terdaftar")
+                    return True
+            return False
+                
+    except FileNotFoundError:
+        print("File Tidak Ditemukan")
+        return False
+
+def register(username, password, role = "Pengunjung"):
     try:
         with open("./Post-Test/Project-Akhir/akun.csv", "a", newline='') as file:
             writer = csv.writer(file)
             writer.writerow([username, password, role])
-
+            print("Register Berhasil")
+    
     except FileNotFoundError:
         print("File Tidak Ditemukan")
 
@@ -19,6 +35,7 @@ def login(username, password):
     try:
         with open("./Post-Test/Project-Akhir/akun.csv", "r") as file:
             reader = csv.reader(file)
+
             for row in reader:
                 if row[0] == username and row[1] == password:
                     print("Login Berhasil")
@@ -40,12 +57,13 @@ def liat_menu():
         with open("./Post-Test/Project-Akhir/menu.csv", "r") as file:
             reader = csv.reader(file)
             lines = list(reader)
+
             if lines:
                 for index, line in enumerate(lines):
                     print(f"""
 Menu ke-{index+1}
 Nama Menu : {line[0]}
-Harga Menu : {line[1]}
+Harga Menu : Rp.{line[1]}
 """)
             else:
                 print("Menu Masih Kosong")
@@ -54,30 +72,51 @@ Harga Menu : {line[1]}
         print("File Tidak Ditemukan")
 
 def tambah_menu(nama_menu, harga_menu):
-    with open("./Post-Test/Project-Akhir/menu.csv", "a", newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([nama_menu, harga_menu])
+    try:
+        with open("./Post-Test/Project-Akhir/menu.csv", "a", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([nama_menu, harga_menu])
+            print("Menu Berhasil Ditambahkan")
+
+    except FileNotFoundError:
+        print("File Tidak Ditemukan")
 
 def ubah_menu(index, menu_baru, harga_baru):
-    with open("./Post-Test/Project-Akhir/menu.csv", "r") as file:
-        lines = list(csv.reader(file))
+    try:
+        with open("./Post-Test/Project-Akhir/menu.csv", "r") as file:
+            lines = list(csv.reader(file))
 
-    if index >= 0 and index < len(lines):
-        lines[index][0] = menu_baru
-        lines[index][1] = harga_baru
-        with open("./Post-Test/Project-Akhir/menu.csv", "w", newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(lines)
+        if 0 <= index < len(lines):
+            lines[index][0] = menu_baru
+            lines[index][1] = harga_baru
+            with open("./Post-Test/Project-Akhir/menu.csv", "w", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(lines)
+            print("Menu Berhasil Diubah")
+
+        else:
+            print("Pilihan Tidak Valid")
+
+    except FileNotFoundError:
+        print("File Tidak Ditemukan")
 
 def hapus_menu(index):
-    with open("./Post-Test/Project-Akhir/menu.csv", "r") as file:
-        lines = list(csv.reader(file))
+    try:
+        with open("./Post-Test/Project-Akhir/menu.csv", "r") as file:
+            lines = list(csv.reader(file))
 
-    if index >= 0 and index < len(lines):
-        del lines[index]
-        with open("./Post-Test/Project-Akhir/menu.csv", "w", newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(lines)
+        if 0 <= index < len(lines):
+            del lines[index]
+            with open("./Post-Test/Project-Akhir/menu.csv", "w", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(lines)
+            print("Menu Berhasil Dihapus")
+
+        else:
+            print("Pilihan Tidak Valid")
+
+    except FileNotFoundError:
+        print("File Tidak Ditemukan")
 
 def kembali_ke_menu():
     print("\nKembali Ke Halaman Utama")
@@ -93,6 +132,30 @@ def tampilan_menu_utama():
 <============================================================>
 """)
 
+def menu_admin():
+    print("\n<==============================>")
+    print("|        SELAMAT DATANG        |")
+    print("<==============================>")
+    print("| 1. Liat Menu                 |")
+    print("| 2. Tambah Menu               |")
+    print("| 3. Ubah Menu                 |")
+    print("| 4. Hapus Menu                |")
+    print("| 5. Kembali                   |")
+    print("<==============================>")
+
+def menu_pengunjung():
+    print("\n<==============================>")
+    print("|        SELAMAT DATANG        |")
+    print("<==============================>")
+    print("| 1. Liat Menu                 |")
+    print("| 2. Tambah Pesanan            |")
+    print("| 3. Ubah Pesanan              |")
+    print("| 4. Hapus Pesanan             |")
+    print("| 5. Lihat Pesanan             |")
+    print("| 6. Kembali                   |")
+    print("<==============================>")
+
+
 def program():
     tampilan_menu_utama()
     pilih1 = input("\nMasukkan Pilihan Anda: ")
@@ -101,9 +164,9 @@ def program():
         clean()
         print("<========== REGISTER ==========>")
         username_baru = input("Masukkan Username Baru: ")
-        password_baru = input("Masukkan Password Baru: ")
-        role_baru = "pengunjung"
-        register(username_baru,password_baru,role_baru)        
+        if not cek_username(username_baru):
+            password_baru = input("Masukkan Password Baru: ")
+            register(username_baru, password_baru)
         print("<==============================>")
 
     elif pilih1 == "2":
@@ -113,67 +176,54 @@ def program():
         password = input("Masukkan Password: ")
         role = login(username,password)
         print("<=====================================>")
-        while log_stat is False:
-            ulangi = input("Apakah Anda Ingin Mencoba Ulang (Y/N): ")
-            if ulangi == "Y":
-                clean()
-                print("<=============== LOGIN ===============>")
-                username = input("Masukkan Username: ")
-                password = input("Masukkan Password: ")
-                role = login(username, password)
-                print("<=====================================>")
-            elif ulangi == "N":
-                clean()
-                print("Kembali Ke Menu Utama")
-                break
-            else:
-                clean()
-                print("Input Salah, Pilihan Hanya (Y/N): ")
-
-        if role:
+        if role == "ADMIN":
             while True:
-                print("\n<==============================>")
-                print("|        SELAMAT DATANG        |")
-                print("<==============================>")
-                print("| 1. Liat Menu                 |")
-                if role == "ADMIN":
-                    print("| 2. Tambah Menu               |")
-                    print("| 3. Ubah Menu                 |")
-                    print("| 4. Hapus Menu                |")
-                print("| 5. Kembali                   |")
-                print("<==============================>")
-
+                menu_admin()
                 pilih2 = input("\nMasukkan Pilihan Anda: ")
                 clean()
+
                 if pilih2 == "1":
                     print("<========== DAFTAR MENU ==========>")
                     liat_menu()
-                    print("\n<=================================>")
+                    print("<=================================>")
 
-                elif pilih2 == "2" and role == "ADMIN":
+                elif pilih2 == "2":
                     print("<========== MENAMBAHKAN MENU ==========>")
                     nama_menu = input("Masukkan Nama Menu: ")
-                    harga_menu = int(input("Masukkan Harga Menu: Rp."))
-                    tambah_menu(nama_menu,harga_menu)
-                    print("Menu Berhasil Ditambahkan")
+                    harga_menu = input("Masukkan Harga Menu: Rp.")
+                    try:
+                        harga_menu = int(harga_menu)
+                        tambah_menu(nama_menu, harga_menu)
+                        print("Menu Berhasil Ditambahkan")
+
+                    except ValueError:
+                        print("Harga harus berupa angka")
                     print("\n<======================================>")
 
-                elif pilih2 == "3" and role == "ADMIN":
+                elif pilih2 == "3":
                     print("<========== MENGUBAH MENU ==========>")
                     liat_menu()
-                    index_baru = int(input("Masukkan Nomor Menu Yang Ingin Diubah: ")) - 1
-                    menu_baru = input("Masukkan Menu Baru: ")
-                    harga_baru = int(input("Masukkan Harga Baru: Rp."))
-                    ubah_menu(index_baru, menu_baru, harga_baru)
-                    print("Menu Berhasil Diubah")
+                    try:
+                        index_baru = int(input("Masukkan Nomor Menu Yang Ingin Diubah: ")) - 1
+                        menu_baru = input("Masukkan Menu Baru: ")
+                        harga_baru = int(input("Masukkan Harga Baru: Rp."))
+                        ubah_menu(index_baru, menu_baru, harga_baru)
+                        print("Menu Berhasil Diubah")
+
+                    except ValueError:
+                        print("Input tidak valid")
                     print("\n<===================================>")
 
-                elif pilih2 == "4" and role == "ADMIN":
+                elif pilih2 == "4":
                     print("<========== MENGHAPUS MENU ==========>")
                     liat_menu()
-                    index_hapus = int(input("Masukkan Nomor Menu Yang Ingin Dihapus: ")) - 1
-                    hapus_menu(index_hapus)
-                    print("Menu Berhasil Dihapus")
+                    try:
+                        index_hapus = int(input("Masukkan Nomor Menu Yang Ingin Dihapus: ")) - 1
+                        hapus_menu(index_hapus)
+                        print("Menu Berhasil Dihapus")
+
+                    except ValueError:
+                        print("Input tidak valid")
                     print("\n<====================================>")
 
                 elif pilih2 == "5":
@@ -188,6 +238,7 @@ def program():
         keluar_dari_program()
 
     else:
+        clean()
         print("\nPilihan Tidak Valid!")
 
 while (True):
